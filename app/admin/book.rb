510618147c14d1ef942b52ffec2a1b46f9856008
saworieza <saworieza@gmail.com>
2014-12-11 15:17:49 +0300

    Creating a Dashboard using active admin

---
+++ app/admin/book.rb
@@ -0,0 +1,26 @@
+ActiveAdmin.register Book do
+index do
+  column 'New Name', :name
+  column :author
+  column :genre
+  column :price do |product|
+      number_to_currency product.price
+    end
+    actions
+end
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # permit_params :list, :of, :attributes, :on, :model
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if resource.something?
+  #   permitted
+  # end
+
+
+end
